d134529b2195c30e1360d0bbd49d12f10cb533cb
Xavier Stamps-Lafont <xavierstampslafont@gmail.com>
2020-03-29 19:14:34 -0400

    Redid menu items

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,34 +1,70 @@
 import React from "react";
 import "./style.scss";
-import { Icon } from "semantic-ui-react";
+import { Icon, SemanticICONS } from "semantic-ui-react";
 
 export default class Menu extends React.Component {
   render() {
+    const items: {
+      label: string;
+      icon: SemanticICONS;
+      link: string;
+      external?: boolean;
+    }[] = [
+      {
+        label: "Home",
+        icon: "home",
+        link: "#home"
+      },
+      {
+        label: "Bio",
+        icon: "user",
+        link: "#bio"
+      },
+      {
+        label: "Work",
+        icon: "code",
+        link: "work"
+      },
+      {
+        label: "Music",
+        icon: "music",
+        link: "https://soundcloud.com/xavierstampslafont",
+        external: true
+      },
+      {
+        label: "Comics",
+        icon: "pencil",
+        link: "https://howsyouroktea.wordpress.com/",
+        external: true
+      },
+      {
+        label: "Email",
+        icon: "mail",
+        link: "mailto:xavierstampslafont@gmail.com",
+        external: true
+      },
+      {
+        label: "LinkedIn",
+        icon: "linkedin",
+        link: "https://www.linkedin.com/in/xavierstampslafont/",
+        external: true
+      }
+    ];
+
     return (
       <div className="components-menu">
-        <span>Work</span>
-        <span>Education</span>
-        <span>Skills</span>
-        <span>
-          <a
-            href="mailto:xavierstampslafont@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Icon name="mail" />
-            xavierstampslafont@gmail.com
-          </a>
-        </span>
-        <span>
-          <a
-            href="https://www.linkedin.com/in/xavierstampslafont/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Icon name="linkedin" />
-            https://www.linkedin.com/in/xavierstampslafont/
-          </a>
-        </span>
+        {items.map(item => (
+          <span key={item.label}>
+            <a
+              href={item.link}
+              target={item.external ? "_blank" : "_self"}
+              rel={item.external ? "noopener noreferrer" : ""}
+            >
+              <Icon name={item.icon} />
+              {item.label}
+            </a>
+          </span>
+        ))}
       </div>
     );
   }
