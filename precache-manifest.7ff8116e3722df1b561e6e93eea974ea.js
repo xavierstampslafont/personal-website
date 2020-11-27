0cb9c36e92c2f3c46200aebcdee7a3fe806bda96
Xavier Stamps-Lafont <xavierstampslafont@gmail.com>
2020-11-27 17:29:02 -0500

    Updates

---
+++ precache-manifest.7ff8116e3722df1b561e6e93eea974ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bffd427ec049be99a828ffee2ba5db11",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "a1d2b5ea263d123795b3",
+    "url": "/personal-website/static/css/main.7f21992b.chunk.css"
+  },
+  {
+    "revision": "1bcb7ec553d7a5babb3d",
+    "url": "/personal-website/static/js/2.72bc38b0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/personal-website/static/js/2.72bc38b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1d2b5ea263d123795b3",
+    "url": "/personal-website/static/js/main.5e52afd2.chunk.js"
+  },
+  {
+    "revision": "f1ce7d685c989d734570",
+    "url": "/personal-website/static/js/runtime-main.68d912c8.js"
+  },
+  {
+    "revision": "fdd94988a0c2d7daf8fa39fd8831bca8",
+    "url": "/personal-website/static/media/portrait.fdd94988.jpg"
+  }
+]);
