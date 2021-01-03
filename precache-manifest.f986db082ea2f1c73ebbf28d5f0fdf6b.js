af2ee5f71d8a049c0fbcc3c3061fe5f5b02bd7f3
Xavier Stamps-Lafont <xavierstampslafont@gmail.com>
2021-01-02 22:10:27 -0500

    Updates

---
+++ precache-manifest.f986db082ea2f1c73ebbf28d5f0fdf6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f4919fa6cb348e040a37640d0a59a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78280a52cd1c77e9be84",
+    "url": "/static/css/main.0894daef.chunk.css"
+  },
+  {
+    "revision": "40f372fee01a5b51c70a",
+    "url": "/static/js/2.e192d2ca.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e192d2ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78280a52cd1c77e9be84",
+    "url": "/static/js/main.a47c4554.chunk.js"
+  },
+  {
+    "revision": "8151b6e8ecddb53b288b",
+    "url": "/static/js/runtime-main.a98ec1ca.js"
+  },
+  {
+    "revision": "fdd94988a0c2d7daf8fa39fd8831bca8",
+    "url": "/static/media/portrait.fdd94988.jpg"
+  }
+]);
