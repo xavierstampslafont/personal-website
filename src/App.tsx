0ff4156847bc013d26b917b99a4b9c16d225e3e1
Xavier Stamps-Lafont <xavier@swiftctrl.com>
2022-05-08 23:00:48 -0400

    Remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import React from "react";
 import "./App.scss";
-import { Menu } from "./components/Menu";
 import { Home } from "./components/Home";
 import { Background } from "./components/Background";
 import { Navigate, Route, Routes } from "react-router-dom";
-import { About } from "./components/About";
-import { Thesis } from "./components/Thesis";
-import { Work } from "./components/Work";
 
 console.warn("TODO: Thesis real sizes for downloads");
 
 const App = (): JSX.Element => (
   <div className="App">
     <Background />
-    <Menu />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="*" element={<Navigate to="/" replace />} />
--- src/components/Divider/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export default function Divider() {
-  // return <p>⬩ ⬥ ⬩</p>;
-  return null;
-}
