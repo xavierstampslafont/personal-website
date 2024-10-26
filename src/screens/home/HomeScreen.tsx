115772cd0bb5af4c9a7775e93c32d90d3dba1701
Xavier Stamps-Lafont <xavierstampslafont@gmail.com>
2024-10-26 11:36:25 -0400

    Update since starting new job soon

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,21 +1,21 @@
-import { intervalToDuration } from "date-fns";
-import React from "react";
-import styled from "styled-components";
-import { ExternalLink } from "../../components";
-import { Portrait } from "./Portrait";
+import { intervalToDuration } from "date-fns"
+import React from "react"
+import styled from "styled-components"
+import { ExternalLink } from "../../components"
+import { Portrait } from "./Portrait"
 
-const FIRST_DEV_WORK = new Date(2011, 0);
+const FIRST_DEV_WORK = new Date(2011, 0)
 const DEV_EXPERIENCE_DURATION = intervalToDuration({
   start: FIRST_DEV_WORK,
   end: new Date(),
-});
+})
 
 export const HomeScreen = () => (
   <StyledHomeScreen>
     <Portrait />
     <BodyText />
   </StyledHomeScreen>
-);
+)
 
 const StyledHomeScreen = styled.div`
   display: flex;
@@ -26,7 +26,7 @@ const StyledHomeScreen = styled.div`
   @media only screen and (min-width: 768px) {
     margin: 0;
   }
-`;
+`
 
 const BodyText = () => (
   <StyledBodyText>
@@ -40,11 +40,11 @@ const BodyText = () => (
     </p>
     <Emoji>💼</Emoji>
     <p>
-      Currently I work at{" "}
-      <ExternalLink to={"https://swiftconnect.io/"}>SwiftConnect</ExternalLink>,
-      where we are developing a new platform that unifies and orchestrates
-      access control, user directory, visitor management, and space management
-      systems. For more on my professional experience, visit my{" "}
+      I have worked in a diverse range of industries, including access
+      management, communication, retail, fintech and video games. In addition, I
+      have worked across different types of companies, from startups to
+      consultancy, and even a unicorn. For more on my professional experience,
+      visit my{" "}
       <ExternalLink to={"https://www.linkedin.com/in/xavierstampslafont/"}>
         LinkedIn profile
       </ExternalLink>
@@ -56,7 +56,7 @@ const BodyText = () => (
       designing digital games for families. I also have a B.A. with double major
       in music composition and Japanese.
     </p>
-    <Emoji>👨‍👩‍👧‍👦</Emoji>
+    <Emoji>🧑‍🧑‍🧒‍🧒</Emoji>
     <p>
       I'm also a husband, and a papa of two. In my free time, I enjoy spending
       time with my family, playing video and board games, and making things.
@@ -78,11 +78,11 @@ const BodyText = () => (
       Countries I've lived in:
       <br />
       <Emoji title="India">🇮🇳</Emoji> <Emoji title="USA">🇺🇸</Emoji>{" "}
-      <Emoji title="France">🇫🇷</Emoji> <Emoji title="Denmark">🇩🇰</Emoji>{" "}
-      <Emoji title="Canada">🇨🇦</Emoji>
+      <Emoji title="Japan">🇯🇵</Emoji> <Emoji title="France">🇫🇷</Emoji>{" "}
+      <Emoji title="Denmark">🇩🇰</Emoji> <Emoji title="Canada">🇨🇦</Emoji>
     </p>
   </StyledBodyText>
-);
+)
 
 const StyledBodyText = styled.div`
   display: flex;
@@ -101,8 +101,8 @@ const StyledBodyText = styled.div`
       height: 100vh;
     }
   }
-`;
+`
 
 const Emoji = styled.span`
   font-size: 1.75em;
-`;
+`
