fe353053332dab99ec67fde6faf5127b32ed46d7
Xavier Stamps-Lafont <xavierstampslafont@gmail.com>
2024-10-26 12:03:19 -0400

    Migrate to Vite

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+
+export default defineConfig({
+  base: "",
+  plugins: [react()],
+  server: {
+    open: true,
+    port: 3000,
+  },
+  build: {
+    outDir: "build",
+  },
+})
