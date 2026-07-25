e0e9e6a370a36badd09630888e086633b7b47cd8
Xavier Stamps-Lafont <xavierstampslafont@gmail.com>
2026-07-24 23:28:17 -0400

    Set up paths in tsconfig and vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import path from "path"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   base: "",
@@ -10,5 +11,10 @@ export default defineConfig({
   },
   build: {
     outDir: "build",
+  },
+  resolve: {
+    alias: {
+      "#": path.resolve(__dirname, "./src"),
+    },
   },
 })
