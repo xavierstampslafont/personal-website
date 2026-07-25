705cb7406d9ba7462ea4c5b7f9b1fc85b6e193e1
Xavier Stamps-Lafont <xavierstampslafont@gmail.com>
2026-07-24 23:10:22 -0400

    Remove unused file serviceWorker.ts

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router"
 import App from "./App.js"
-import * as serviceWorker from "./serviceWorker.js"
 
 const container = document.getElementById("root")
 
@@ -15,8 +14,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>,
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
